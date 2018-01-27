ea950576da485512c9188194c7bbd61068a35372
HoldYourWaffle <ravivanrooijen@live.nl>
2018-01-27 17:43:41 +0100

    Initial commit

---
+++ src/main/java/info/zthings/mcmods/PLACEMODIDHERE/proxy/CommonProxy.java
@@ -0,0 +1,9 @@
+package info.zthings.mcmods.PLACEMODIDHERE.proxy;
+
+import net.minecraft.item.Item;
+
+public class CommonProxy {
+
+	public void registerItemRenderer(Item baseItem, int i, String name) {}
+	
+}
